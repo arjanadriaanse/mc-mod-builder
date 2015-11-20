a0f68bc796a77aa8c07e326d808640b7083e3fe5
Arjan Adriaanse <arjan@adriaan.se>
2015-11-20 20:18:47 +0100

    begin

---
+++ src/main/java/twintro/minecraft/modbuilder/BuilderMod.java
@@ -0,0 +1,18 @@
+package twintro.minecraft.modbuilder;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = BuilderMod.MODID, version = BuilderMod.VERSION)
+public class BuilderMod
+{
+    public static final String MODID = "modbuilder";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        
+    }
+}
