04c9415ee4a577a9cfd0b04c553578c7bb430b67
trijemusk <jorkedevlas@gmail.com>
2015-11-23 12:40:43 +0100

    Peeled Eggs is nu een item

--- src/main/java/example/items/MyItems.java
+++ src/main/java/example/items/MyItems.java
@@ -7,16 +7,19 @@ import net.minecraftforge.fml.relauncher.Side;
 public class MyItems {
 	public static Item wand;
 	public static Item cookedEgg;
+	public static Item peeledEgg;
 	
 	public static void preInit(){
 		wand = new ItemWand();
 		cookedEgg = new ItemCookedEgg();
+		peeledEgg = new ItemPeeledEgg();
 	}
 	
 	public static void init(FMLInitializationEvent event){
 		if (event.getSide() == Side.CLIENT){
 			((ItemWand)wand).renderItem();
 			((ItemCookedEgg)cookedEgg).renderItem();
+			((ItemPeeledEgg)peeledEgg).renderItem();
 		}
 	}
 }
