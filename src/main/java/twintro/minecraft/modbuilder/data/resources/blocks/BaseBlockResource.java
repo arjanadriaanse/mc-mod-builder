95fea96dfb6cffe992b51c74b501c56292a023df
trijemusk <jorkedevlas@gmail.com>
2015-12-09 15:37:40 +0100

    enkele variabelen van blocks worden gezet vanuit het resource pack.

--- src/main/java/twintro/minecraft/modbuilder/data/resources/blocks/BaseBlockResource.java
+++ src/main/java/twintro/minecraft/modbuilder/data/resources/blocks/BaseBlockResource.java
@@ -8,4 +8,10 @@ public abstract class BaseBlockResource {
 	public MaterialResource material;
 	public String model;
 	public TabResource tab;
+	
+	public Integer light; //light level (0-15)
+	public Integer opacity; //geeft aan hoeveel licht door het block wordt doorgelaten (0-15)
+	public Float slipperiness; //gladheid van het block
+	public Float hardness; //hoe lang het duurt om het block kapot te maken
+	public Boolean unbreakable;
 }
