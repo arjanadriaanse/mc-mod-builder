a3a32e3e1e604257501d6c845b4a030ee6dd2bd6
M.P.V. <mikedevries.smiley@gmail.com>
2015-12-09 17:04:34 +0100

    Interface beginselen gemaakt

--- src/main/java/twintro/minecraft/modbuilder/editor/Editor.java
+++ src/main/java/twintro/minecraft/modbuilder/editor/Editor.java
@@ -12,6 +12,50 @@ import twintro.minecraft.modbuilder.data.resources.MaterialResource;
 import twintro.minecraft.modbuilder.data.resources.TabResource;
 import twintro.minecraft.modbuilder.data.resources.blocks.BlockResource;
 import twintro.minecraft.modbuilder.editor.generator.ResourcePackGenerator;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JTree;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JDesktopPane;
+import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JSplitPane;
+import java.awt.GridLayout;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JToolBar;
+import javax.swing.JInternalFrame;
+import javax.swing.JLayeredPane;
+import java.awt.FlowLayout;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import java.awt.SystemColor;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.ScrollPaneConstants;
+import java.awt.CardLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JScrollBar;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
 
 public class Editor {
 	public static final String modName = "exampleName";
@@ -20,6 +64,12 @@ public class Editor {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -60,8 +110,83 @@ public class Editor {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		//Window
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 900, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Menubar
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(SystemColor.inactiveCaption);
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmNew = new JMenuItem("New");
+		mnFile.add(mntmNew);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnHelp.add(mntmAbout);
+		
+		//Main panel
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setBackground(SystemColor.control);
+		frame.getContentPane().add(splitPane, BorderLayout.CENTER);
+		
+		//Button panel
+		JPanel buttonPanel = new JPanel();
+		splitPane.setLeftComponent(buttonPanel);
+		buttonPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		ActionListener buttonListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				changePanel(((JButton)e.getSource()).getText());
+			}
+		};
+		
+		JButton TexturesButton = new ActivityButton("Textures");
+		TexturesButton.addActionListener(buttonListener);
+		buttonPanel.add(TexturesButton);
+		
+		JButton RecipesButton = new ActivityButton("Recipes");
+		RecipesButton.addActionListener(buttonListener);
+		buttonPanel.add(RecipesButton);
+		
+		JButton BlocksButton = new ActivityButton("Blocks");
+		BlocksButton.addActionListener(buttonListener);
+		buttonPanel.add(BlocksButton);
+		
+		JButton ItemsButton = new ActivityButton("Items");
+		ItemsButton.addActionListener(buttonListener);
+		buttonPanel.add(ItemsButton);
+		
+		//Activity panel
+		JPanel ActivityPanel = new JPanel();
+		splitPane.setRightComponent(ActivityPanel);
+		ActivityPanel.setLayout(new CardLayout(0, 0));
+		activityPanel = ActivityPanel;
+		
+		JPanel TexturePanel = new ActivityPanel("Textures", "New Texture");
+		ActivityPanel.add(TexturePanel, "Textures");
+		
+		JPanel RecipePanel = new ActivityPanel("Recipes", "New Recipe");
+		ActivityPanel.add(RecipePanel, "Recipes");
+		
+		JPanel BlockPanel = new ActivityPanel("Blocks", "New Block");
+		ActivityPanel.add(BlockPanel, "Blocks");
+		
+		JPanel ItemPanel = new ActivityPanel("Items", "New Item");
+		ActivityPanel.add(ItemPanel, "Items");
+	}
+	
+	JPanel activityPanel;
+	
+	private void changePanel(String panel){
+		CardLayout cl = (CardLayout)(activityPanel.getLayout());
+		cl.show(activityPanel, panel);
 	}
 }
