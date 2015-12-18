f433cc5efc47113902a68466ec331c960412b5da
M.P.V. <mikedevries.smiley@gmail.com>
2015-12-18 12:29:43 +0100

    addItem in itemsactivitypanel toegevoegd

--- src/main/java/twintro/minecraft/modbuilder/editor/interfaces/ItemsActivityPanel.java
+++ src/main/java/twintro/minecraft/modbuilder/editor/interfaces/ItemsActivityPanel.java
@@ -1,37 +1,44 @@
 package twintro.minecraft.modbuilder.editor.interfaces;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.swing.JOptionPane;
 
 import twintro.minecraft.modbuilder.editor.ActivityPanel;
+import twintro.minecraft.modbuilder.editor.generator.ResourcePackGenerator;
 import twintro.minecraft.modbuilder.editor.resources.ItemElement;
 
 public class ItemsActivityPanel extends ActivityPanel {
 	public ItemsActivityPanel(String header, String button) {
 		super(header, button);
 	}
-	
-	private List<String> models;
 
 	@Override
 	protected void add() {
 		String name = JOptionPane.showInputDialog("Item name:");
 		if (name != null)
 			if (name.replaceAll(" ", "").length() > 0)
-				new ItemEditor(models);
+				new Object();//TODO
 	}
 	
-	public void addItem(ItemElement item){
-		for (String model : elements.keySet()){
-			
+	public void addItem(ItemElement element){
+		createFile(element.itemModel, "assets/modbuilder/models/item/" + element.name + ".json");
+		createFile(element.item, "assets/modbuilder/items/" + element.name + ".json");
+		addElement(element.name, element.image);
+	}
+	
+	public void createFile(Object model, String dir){
+		try {
+			ResourcePackGenerator.createFile(model, dir);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
 	@Override
 	protected void edit() {
 		String value = (String) list.getSelectedValue();
-	//	ItemEditor editor = new ItemEditor(value);
 	}
 	
 	@Override
