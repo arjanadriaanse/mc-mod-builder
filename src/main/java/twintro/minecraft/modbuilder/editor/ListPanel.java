efbe65d6ed3ea7e928736ad2fe7dc0e2bceee16a
M.P.V. <mikedevries.smiley@gmail.com>
2015-12-22 23:07:30 +0100

    listpanel add to index :I

---
+++ src/main/java/twintro/minecraft/modbuilder/editor/ListPanel.java
@@ -0,0 +1,10 @@
+package twintro.minecraft.modbuilder.editor;
+
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class ListPanel extends JPanel {
+	public Map<String, ImageIcon> elements;
+}
